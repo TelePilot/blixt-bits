b47bcbe3cb585f918c5051cd2f3c54803be8fee8
Blixt <teodor.blixt@gmail.com>
2019-07-31 19:23:17 +0100

    update menu items with routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import "./menu-item.styles.scss"
+import { withRouter } from 'react-router-dom';
 
-export const MenuItem = ( {title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+import "./menu-item.styles.scss";
+
+export const MenuItem = ( {title, imageUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className="background-image" style={{
             backgroundImage: `url(${imageUrl})`
             }}>
@@ -15,4 +17,4 @@ export const MenuItem = ( {title, imageUrl, size }) => (
 
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
