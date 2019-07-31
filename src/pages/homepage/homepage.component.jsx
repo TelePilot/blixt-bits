7a6846232369550769fb7179d210f3b1229de570
Blixt <teodor.blixt@gmail.com>
2019-07-31 18:17:07 +0100

    broken everything into smaller components and styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss'
+import Directory from '../../components/directory/diretory.component'
+
+const HomePage = () => (
+    <div className='homepage'>
+      <h1>Blixt-mof-Bits</h1>
+      <Directory/>
+    </div>
+
+)
+
+export default HomePage
