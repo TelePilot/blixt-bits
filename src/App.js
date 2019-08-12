4d9046ca7f112793efcdb5f53bb0b7532b153d18
Blixt <teodor.blixt@gmail.com>
2019-08-12 14:51:48 +0100

    finishing sign in and sign up

--- src/App.js
+++ src/App.js
@@ -31,9 +31,8 @@ class App extends React.Component {
              id: snapShot.id,
              ...snapShot.data()
            }
-         }, () => {
-          console.log(this.state)
-         })
+         }
+         )
          
        })
       
