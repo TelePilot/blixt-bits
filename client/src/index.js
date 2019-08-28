377404bf3a573c038d3d6d4e903ef169315e1162
Blixt <teodor.blixt@gmail.com>
2019-08-28 16:29:53 +0100

    updated our code to be PWA complient and force reroutes from http to https with express-sslify

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from "redux-persist/integration/react"
 
 import {store, persistor} from './redux/store'
+import * as serviceWorker from './serviceWorker'
 
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './App'
 
 
 
@@ -20,5 +21,6 @@ ReactDOM.render(
         </BrowserRouter>
     </Provider>
     
-, document.getElementById('root'));
+, document.getElementById('root'))
 
+serviceWorker.register()
